0b652807e85dda99ad1f4791e1095aa2ebd6df97
RITISH25 <60220397+RITISH25@users.noreply.github.com>
2021-01-11 18:59:00 +0530

    Create SpeakHello.js

---
+++ module 4 solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window){
+	var speakWord = "Hello";
+	var helloSpeaker=new Object();
+	helloSpeaker.speak=function speak(name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker=helloSpeaker;
+})(window);
